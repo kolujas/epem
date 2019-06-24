56cb3d31b2ddc2c22827f8cf2bb00bd3add31379
kolujAs <fernandodeibe@gmail.com>
2019-06-24 03:30:19 -0300

    etiquetas para ignorar el cache

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -9,6 +9,10 @@
 
         <!--Let browser know website is optimized for mobile-->
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta http-equiv="Expires" content="0">
+        <meta http-equiv="Last-Modified" content="0">
+        <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+        <meta http-equiv="Pragma" content="no-cache">
 
         <!--CSS-->
         <link href="{{ asset('css/index.css') }}" rel="stylesheet">
